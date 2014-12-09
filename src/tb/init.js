976631bc9d3c2ab4609e508598fadca79ac13eea
Florian Kroockmann <florian.kroockmann@lp-digital.fr>
2014-12-09 16:44:12 +0100

    #3783 start DOMParser for content

--- src/tb/init.js
+++ src/tb/init.js
@@ -34,11 +34,11 @@ define(['jquery'], function (jQuery) {
                 if (!event.altKey || !event.ctrlKey || 66 !== event.keyCode) {
                     return;
                 }
-                this.load();
+                this.load(false);
             }
         },
 
-        load: function () {
+        load: function (already_connected) {
             var self = this;
             require(['tb.core', 'src/tb/config'], function (Core, config) {
 
@@ -55,15 +55,19 @@ define(['jquery'], function (jQuery) {
                     router.navigate(app.getMainRoute());
                 });
 
-                Core.Mediator.subscribe('onSuccessLogin', function () {
-                    self.toolBarDisplayed = true;
-                    /*
-                     * @TODO: Load config by a rest when user connected
-                     */
+                if (true === already_connected) {
                     Core.initConfig(config);
-                });
+                } else {
+                    Core.Mediator.subscribe('onSuccessLogin', function () {
+                        self.toolBarDisplayed = true;
+                        /*
+                         * @TODO: Load config by a rest when user connected
+                         */
+                        Core.initConfig(config);
+                    });
 
-                Core.authentication.showForm();
+                    Core.authentication.showForm();
+                }
 
             }, this.onError);
         },
